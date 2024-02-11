8002ed17c0e35570363ba77b7565d93aa1d9ee30
Melih Yesilyurt <Melih58Yesilyurt@gmail.com>
2024-02-11 20:36:48 +0300

    Initial Commit

---
+++ CurrencyManagement/CurrencyManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyManagementApp.swift
+//  CurrencyManagement
+//
+//  Created by Melih Yesilyurt on 11.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
